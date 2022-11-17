e813f1c62cdf93269769a6c4ae69a28e7ea37ba9
Ivan Rogério <ivanfilho22@icloud.com>
2022-11-17 17:58:53 -0300

    Responsividade: tela criar usuário

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -21,20 +21,20 @@ export default function CreateUser() {
 				{/* SIDEBAR */}
 				<Sidebar />
 
-				<Box flex={'1'} borderRadius={8} bg={'gray.800'} p={'8'}>
+				<Box flex={'1'} borderRadius={8} bg={'gray.800'} p={['6', '8']}>
 					<Heading size={'lg'} fontWeight={'normal'}>
 						Criar usuário
 					</Heading>
 
-					<Divider my={'6'} borderColor={'gray.700'} />
+					<Divider my={['6', '8']} borderColor={'gray.700'} />
 
-					<VStack spacing={'8'}>
-						<SimpleGrid minChildWidth={'240px'} spacing={'8'} w={'100%'}>
+					<VStack spacing={['6', '8']}>
+						<SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w={'100%'}>
 							<Input name={'name'} label={'Nome'} />
 							<Input name={'email'} type={'email'} label={'E-mail'} />
 						</SimpleGrid>
 
-						<SimpleGrid minChildWidth={'240px'} spacing={'8'} w={'100%'}>
+						<SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w={'100%'}>
 							<Input name={'password'} type={'password'} label={'Senha'} />
 							<Input
 								name={'password_confirmation'}
